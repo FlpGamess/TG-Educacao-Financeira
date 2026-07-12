f6bde6b61529ee42b6aacb376adf5a9d91432a64
Caiovj18 <caiocc18022004@gmail.com>
2026-07-12 20:47:10 -0300

    Lógica de salario

--- Assets/Scripts/ModuloTempo.cs
+++ Assets/Scripts/ModuloTempo.cs
@@ -4,14 +4,14 @@ using UnityEngine;
 
 public class ModuloTempo : MonoBehaviour
 {
-    public int semana = 0;
+    public static int semana = 0;
     public TextMeshProUGUI semanav;
     public static event Action isSemanaAvancada;
 
     //caso o evento seja ativado
     private void OnEnable()
     {
-        //adiciona na fila do evento isEsgotado a função atualizar semana
+        //adiciona na fila do evento isEsgotado a fun  o atualizar semana
         ModuloDisposicao.isEsgotado += AtualizarSemana;
     }
     //quando o evento acaba vulgo desativado
@@ -36,8 +36,8 @@ public class ModuloTempo : MonoBehaviour
     //atualiza a semana, resumidamente semana+=1
     public void AtualizarSemana()
     {
-        isSemanaAvancada?.Invoke();
         semana++;
+        isSemanaAvancada?.Invoke();
         semanav.text = "Semana"+semana.ToString() ;
     }
 
